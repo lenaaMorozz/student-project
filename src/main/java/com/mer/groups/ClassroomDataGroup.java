a8f0d1637f61c35b6e3b033241d599eddd10cabb
lenaaMorozz <lenaamorozz@gmail.com>
2023-10-23 12:07:24 +0500

    added abstract class and refactored classroom

--- src/main/java/com/mer/groups/ClassroomDataGroup.java
+++ src/main/java/com/mer/groups/ClassroomDataGroup.java
@@ -1,52 +1,45 @@
 package com.mer.groups;
 
 import com.mer.Student;
-import java.util.ArrayList;
 
-public class ClassroomDataGroup<K extends Integer, V> implements DataGroup<K, V> {
+public class ClassroomDataGroup<K extends Integer, V extends MyLinkedList<Student>> extends DataGroupAbstr<K, V> {
 
-    private final Node<Integer, ArrayList<Student>>[] table = new Node[11];
 
-    {
-        for (int i = 0; i < 11; i++) {
-            table[i].key = i + 1;
-        }
+    public ClassroomDataGroup() {
+        setSize(11);
+        initialTable();
+//        for (int i = 0; i < 11; i++) {
+//            getNode(i).key = (K) Integer.valueOf(i + 1);
+//        }
     }
+
 
     @Override
     public void addStudent(Student student) {
         switch (student.getGroup()) {
-            case 1 -> table[0].value.add(student);
-            case 2 -> table[1].value.add(student);
-            case 3 -> table[2].value.add(student);
-            case 4 -> table[3].value.add(student);
-            case 5 -> table[4].value.add(student);
-            case 6 -> table[5].value.add(student);
-            case 7 -> table[6].value.add(student);
-            case 8 -> table[7].value.add(student);
-            case 9 -> table[8].value.add(student);
-            case 10 -> table[9].value.add(student);
-            case 11 -> table[10].value.add(student);
+            case 1 -> getTable()[0].value.add(student);
+            case 2 -> getTable()[1].value.add(student);
+            case 3 -> getTable()[2].value.add(student);
+            case 4 -> getTable()[3].value.add(student);
+            case 5 -> getTable()[4].value.add(student);
+            case 6 -> getTable()[5].value.add(student);
+            case 7 -> getTable()[6].value.add(student);
+            case 8 -> getTable()[7].value.add(student);
+            case 9 -> getTable()[8].value.add(student);
+            case 10 -> getTable()[9].value.add(student);
+            case 11 -> getTable()[10].value.add(student);
         }
     }
 
     @Override
-    public Student[] getStudents(Integer key) {
-        return get(key).toArray(Student[]::new);
+    public MyLinkedList<Student> getStudents(Integer key) {
+        return getTable()[key - 1].value;
     }
 
-    private ArrayList<Student> get(Integer key) {
-        Node<Integer, ArrayList<Student>> node = table[key - 1];
-        return node.value;
-    }
-
-    static class Node<K, V> {
-        K key;
-        V value;
-
-        Node(K key, V value) {
-            this.key = key;
-            this.value = value;
+    @Override
+    protected void initialTable() {
+        for (int i = 0; i < 11; i++) {
+            getTable()[i] = (Node<K, V>) new Node<>(i + 1, new MyLinkedList<Student>());
         }
     }
 }
---
+++ src/main/java/com/mer/groups/DataGroupAbstr.java
@@ -0,0 +1,30 @@
+package com.mer.groups;
+
+
+import com.mer.Student;
+
+public abstract class DataGroupAbstr<K, V> implements DataGroup<K, V> {
+    private Node<K, V>[] table;
+
+    protected abstract void initialTable();
+
+
+    protected void setSize(int size) {
+        table = new Node[size];
+    }
+
+
+    protected Node<K,V>[] getTable() {
+        return table;
+        }
+
+    protected static class Node<K, V> {
+        K key;
+        V value;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
