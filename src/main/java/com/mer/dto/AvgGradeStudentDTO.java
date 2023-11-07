e4a984775e25474e825cbd3767649d03900ac69a
lenaaMorozz <lenaamorozz@gmail.com>
2023-11-07 23:43:02 +0500

    update AvgGradeStudentDTO

--- src/main/java/com/mer/dto/AvgGradeStudentDTO.java
+++ src/main/java/com/mer/dto/AvgGradeStudentDTO.java
@@ -1,22 +1,25 @@
 package com.mer.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.StringJoiner;
 
+@Getter
 @AllArgsConstructor
 public class AvgGradeStudentDTO {
     private String firstName;
     private String lastName;
-    private int group;
+//    private int group;
     private double avgGrade;
 
-    @Override
-    public String toString() {
-        return new StringJoiner(", ")
-                .add("firstName='" + firstName + "'")
-                .add("lastName='" + lastName + "'")
-                .add("group=" + group)
-                .add("avgGrade=" + avgGrade)
-                .toString();
-    }
+//    @Override
+//    public String toString() {
+//        return new StringJoiner(", ")
+//                .add("firstName='" + firstName + "'")
+//                .add("lastName='" + lastName + "'")
+//                .add("group=" + group)
+//                .add("avgGrade=" + avgGrade)
+//                .toString();
+//    }
 }
